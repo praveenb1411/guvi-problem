302b3b5c66461adca48557e12e22707a9027608b
praveenb1411 <51699308+praveenb1411@users.noreply.github.com>
2019-06-17 14:16:35 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
- 
+
 int main(void) {
 	int n;
 	scanf("%d",&n);
+	if(n<0)
+	{printf("invalid");
+	}
+	else
+	{
 	if(n%2==0)
 	printf("Even");
 	else
-	printf("Odd");
+	printf("Odd");}
 	// your code goes here
 	return 0;
 }
