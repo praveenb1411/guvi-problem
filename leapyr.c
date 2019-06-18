25b8a616ca13246ffb9ed6b681400e80c3e6eaaf
praveenb1411 <51699308+praveenb1411@users.noreply.github.com>
2019-06-18 10:04:15 +0530

    Update leapyr.c

--- leapyr.c
+++ leapyr.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main()
-{
+{int year;
+scanf("%d",&year);
    if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-      printf("yes");
+    printf("yes");
    else
       printf("no");
     return 0;
