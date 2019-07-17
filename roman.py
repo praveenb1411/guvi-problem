1118c58ed179751bd562f390bc28326be7173e0c
praveenb1411 <51699308+praveenb1411@users.noreply.github.com>
2019-07-17 13:59:07 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,6 @@
+m=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+n=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+g=(input())
+for o in range(20):
+    if(g==m[o]):
+        print(n[o])
